4fbee2da5332b906d3215f7db32c3eb6cd3cd4c0
mohid <mohid10587@gmail.com>
2022-10-05 16:57:49 +0600

    Slider in nextjs app   with react-slick

---
+++ components/data.js
@@ -0,0 +1,102 @@
+
+
+export const dataDigitalBestSeller = [
+    {
+      id: 1,
+      title: ' 1 Mario Kart™ 8 Deluxe',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        '/hero.avif',
+    },
+    {
+      id: 2,
+      title: '2 TRIANGLE STRATEGY™',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        '/pic2.avif',
+    },
+    {
+      id: 3,
+      title: '3 Pokémon™ Legends: Arceus',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic3.avif',
+    },
+    {
+      id: 4,
+      title: '4 Super Mario™ 3D World  ',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic4.avif',
+    },
+    {
+      id: 5,
+      title: '5 Cuphead',
+      price: '$19.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic5.avif',
+    },
+    {
+      id: 6,
+      title: '6 Minecraft',
+      price: '$29.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic6.avif',
+    },
+    {
+      id: 7,
+      title: '7 Mario + Rabbids® Kingdom ',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic7.avif',
+    },
+    {
+      id: 8,
+      title: '8 Unravel Two',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      sale: 63, //percent
+      linkImg:
+      '/pic8.avif',
+    },
+    {
+      id: 5,
+      title: '9 Cuphead',
+      price: '$19.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic2.avif',
+    },
+    {
+      id: 6,
+      title: '10 Minecraft',
+      price: '$29.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic3.avif',
+    },
+    {
+      id: 7,
+      title: '11 Mario + Rabbids® Kingdom',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      '/pic4.avif',
+    },
+    {
+      id: 8,
+      title: '12 Unravel Two',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      sale: 63, //percent
+      linkImg:
+      '/pic5.avif',
+    },
+  ];
